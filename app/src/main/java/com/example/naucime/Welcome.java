f193193f53d9f9030b0425748b97b2cab5f022aa
Milos Jovovic <loshmyfit@gmail.com>
2020-04-15 03:18:35 +0200

    dodate pozadine predmetima, nisu podesene slike dodat welcome screen

---
+++ app/src/main/java/com/example/naucime/Welcome.java
@@ -0,0 +1,30 @@
+package com.example.naucime;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+public class Welcome extends AppCompatActivity {
+    private ImageView logo;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+        logo = findViewById(R.id.logo);
+        Animation animation = AnimationUtils.loadAnimation(this,R.anim.welcome_screen);
+        logo.startAnimation(animation);
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(Welcome.this, MainActivity.class);
+                startActivity(intent);
+            }
+        }, 2000);
+    }
+}
